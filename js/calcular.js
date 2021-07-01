0c65c35061675bfbef2eb2a678733fa2a73b7446
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2021-06-30 20:07:26 -0500

    Comentarios.
    Se agregaron comentario de como funciona la página.

--- js/calcular.js
+++ js/calcular.js
@@ -1,30 +1,31 @@
-function contenido(id) {
+function contenido(id) { //se denomina la función que va a retornar todo el proceso con los ID.
   return document.getElementById(id);
 }
-function calcularProm(){
-  var pn= parseInt(contenido('1nota').value);
-  var sn= parseInt(contenido('2nota').value);
-  var tn= parseInt(contenido('3nota').value);
-  var cn= parseInt(contenido('4nota').value);
-  var total = (pn + sn + tn + cn )/4;
-  contenido('media').value = total;
+function calcularProm(){ //se denomina la función para calcular el promedio
+  var pn= parseInt(contenido('1nota').value); //la variable pn es de la primer nota
+  var sn= parseInt(contenido('2nota').value); //la variable sn es de la segunda nota
+  var tn= parseInt(contenido('3nota').value); //la variable tn es de la tercer nota
+  var cn= parseInt(contenido('4nota').value); //la variable cn es de la cuarta nota
+  var total = (pn + sn + tn + cn )/4; //es la operación que se realiza para calcular el promedio
+  contenido('media').value = total; //la variable del resultado (total) se guarda en el ID media
 
-  if (total >=90 & total <=100) {
-    contenido('EQ').value = 'A';
+//SE COLOCA LAS CONDICIONES PARA DEFINIR EL EQ DEL PROMEDIO
+  if (total >=90 & total <=100) { // si total es mayor o igual a 90 o menor o igual a 100
+    contenido('EQ').value = 'A'; //La calificación es de A
   }
-  else if (total >=80 & total <=90) {
-    contenido('EQ').value = 'B';
+  else if (total >=80 & total <=90) { // si total es mayor o igual a 80 o menor o igual a 90
+    contenido('EQ').value = 'B'; //La calificación es de B
   }
-  else if (total >=70 & total <=80) {
-    contenido('EQ').value = 'C';
+  else if (total >=70 & total <=80) { // si total es mayor o igual a 70 o menor o igual a 80
+    contenido('EQ').value = 'C'; //La calificación es de C
   }
-  else if (total >=60 & total <=70) {
-    contenido('EQ').value = 'D';
+  else if (total >=60 & total <=70) { // si total es mayor o igual a 60 o menor o igual a 70
+    contenido('EQ').value = 'D'; //La calificación es de B
   }
-  else if (total <60) {
-    contenido('EQ').value = 'F';
+  else if (total <60) { // si total es menor a 60
+    contenido('EQ').value = 'F'; //La calificación es de F
   }
-  else {
-    contenido('EQ').value = 'Invalido';
+  else { //si por error se coloca otro caracter que no sea número
+    contenido('EQ').value = 'Dato Inválido'; //mostrará que es un dato Inválido.
   }
 }
